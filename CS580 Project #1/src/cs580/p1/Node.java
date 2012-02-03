1668fcbd1890d2573284f83326ec2ef9e8b6aa0e
Mike Ballard <mike.ballard@gmail.com>
2012-02-02 18:11:37 -0800

    Slight Redesign.
    Edges variables now holds Node objects instead of string objects
    denoting the connection.  Added some additional documentation.

--- CS580 Project #1/src/cs580/p1/Node.java
+++ CS580 Project #1/src/cs580/p1/Node.java
@@ -1,3 +1,7 @@
+/**
+ * @author Mike Ballard
+ * @version Alpha
+ */
 package cs580.p1;
 
 import java.util.LinkedList;
@@ -16,10 +20,26 @@ final class Node {
                       doNode         = false;
    // Zero indexed.
    int                predicateCount = 0;
-   LinkedList<String> edges          = new LinkedList<String>();
+   /*
+    * Unidirectional edges. We will assume that an edge goes from the current
+    * node to all of the nodes in the edge list.
+    */
+   LinkedList<Node> edges          = new LinkedList<Node>();
    Node               previous       = null;
-
+   /**
+    * Constructor.
+    * 
+    * @param lineNumber
+    */
    public Node(int lineNumber) {
       this.lineNumber = lineNumber;
+   }
+   /**
+    * Adds the passed Node object to the current objects edge list.
+    * 
+    * @param towards
+    */
+   public void addEdge(Node towards){
+      edges.addLast(towards);
    }
 }
