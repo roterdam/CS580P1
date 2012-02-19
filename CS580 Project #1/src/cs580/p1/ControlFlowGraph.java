dae7fadf6583e042061f89120e1b4c959e5e8dbf
Mike Ballard <Mike.Ballard@gmail.com>
2012-02-18 19:07:39 -0800

    The curious case of case. I don't quite remember exactly all that has changed.  I know I have replaced a lot of if-else (and subsequent nests thereof) with case statements.

--- CS580 Project #1/src/cs580/p1/ControlFlowGraph.java
+++ CS580 Project #1/src/cs580/p1/ControlFlowGraph.java
@@ -14,7 +14,8 @@ import java.util.Stack;
  */
 public class ControlFlowGraph {
    //What we will be using to parse against.
-   String regex = ".*(if|else|while|do|\\}|return|for).*";
+   String regex = ".*(if|else|while|do|return|for).*";
+   String endControlToken="}";
    //graph will store all of the nodes generated from parsing.
    LinkedList<Node> graph = new LinkedList<Node>();
    /*
@@ -35,24 +36,93 @@ public class ControlFlowGraph {
       boolean correctParse=true;
       String str = line.toLowerCase();
       //TODO: Parsing stuff.
-      if(!lastControlStatement.isEmpty()) {
-         //Got something on the stack.
-         Node stack=lastControlStatement.peek();
-         if(stack.type==Node.SIMPLE_NODE||stack.type==Node.DUMMY_NODE) {
-            //TODO: Finish
-         }
-         else {
-            Node struct = generateStructure(whichCtrlStmt(str),lineNumber);
-            stack.nest(struct);
-            lastControlStatement.push(struct);
-            //TODO: finish
-         }
+      switch(whichCtrlStmt(str)) {
+         case Node.DO_NODE:
+            break;
+         case Node.IF_NODE:
+            break;
+         case Node.RETURN_NODE:
+            break;
+         case Node.WHILE_NODE:
+            break;
+         default:
+            //Check to see if we have an end of control statement token.
+            if(str.contains(endControlToken)) {
+               /* Whatever it was, should now be done.  The only case that this
+                * would be false is if there is nothing on the stack, which
+                * would mean that we went through a block and it contained only
+                * procedure statements.
+                */
+               if(lastControlStatement.isEmpty()) break; //Okay to discard.
+               else {
+                  /* So the last control block has finished and should be
+                   * removed from the stack, but we want to keep a ghost there
+                   * so if there something that does come after (a sequence) we
+                   * can keep it linked correctly.  For this, we will create a
+                   * dummy node, whose edge list will point to the current
+                   * control node.
+                   */
+                  Node dummy=generateStructure(Node.DUMMY_NODE,-1,-1);
+                  dummy.addEdge(lastControlStatement.pop().exitNode);
+                  lastControlStatement.push(dummy);
+               }
+            } else {
+               //the line passed is a procedure node.
+               //Call a function that modifies the stack in some manner as to process this currently line.
+               process(lastControlStatement,Node.SIMPLE_NODE,lineNumber,str);
+            }
       }
       return correctParse;
    }
    
+   private void process(Stack<Node> stack, int nodeType, int lineNumber, String line) {
+      if(!stack.isEmpty()){
+         Node lastAct=stack.peek();
+         switch(lastAct.type){
+            case Node.DO_NODE:
+               //TODO: Finish if stack==do
+               break;
+            case Node.DUMMY_NODE:
+               //TODO: Finish if stack==dummy
+               break;
+            case Node.IF_NODE:
+               //TODO: Finish if stack==if, fix for else
+               Node struct=generateStructure(nodeType,lineNumber);
+               lastAct.setExit(struct);
+               
+               break;
+            case Node.SIMPLE_NODE:
+               //TODO: Finish if stack==simple
+               break;
+            case Node.WHILE_NODE:
+               //TODO: Finish if stack==while
+               break;
+            default:
+         }//End Switch
+      }//End If
+      else {
+         Node struct = generateStructure(nodeType,lineNumber);
+         stack.push(struct);
+      }//End Else
+      
+      /*
+      if(!lastControlStatement.isEmpty()) {
+         //Got something on the stack.
+         Node stacker=lastControlStatement.peek();
+         if(stacker.type==Node.SIMPLE_NODE) {
+            //TODO: Finish
+         }
+         else {
+            Node struct = generateStructure(whichCtrlStmt(""),lineNumber);
+            stacker.nest(struct);
+            lastControlStatement.push(struct);
+            //TODO: finish
+         }
+      }*/
+   }//End Funct
+   
    private int whichCtrlStmt(String line){
-      int returnVal=Node.SIMPLE_NODE;
+      int returnVal=-1;
       if(line.contains("if")) returnVal=Node.IF_NODE;
       else if(line.contains("do")) returnVal=Node.DO_NODE;
       else if(line.contains("while")) returnVal=Node.WHILE_NODE;
