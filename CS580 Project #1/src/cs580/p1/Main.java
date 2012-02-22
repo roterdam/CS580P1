aeed985216e59af3fc2178e9184f2a09c7cdfbf7
Mike Ballard <Mike.Ballard@gmail.com>
2012-02-21 22:25:51 -0800

    Finished Main. Main reads data in via | or <filename from the terminal.

--- CS580 Project #1/src/cs580/p1/Main.java
+++ CS580 Project #1/src/cs580/p1/Main.java
@@ -7,16 +7,26 @@ package cs580.p1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 @SuppressWarnings("unused")
 public class Main {
 	public static void main(String[] arg) {
-	   //Testing.
-	   System.out.println(" ");
 	   ControlFlowGraph cfg = new ControlFlowGraph();
-	   cfg.parse("Test", 0);
-	   cfg.parse("Test", 0);
+	   BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	   try {
+	      for(int lineNumber=1; true; lineNumber++) {
+	         String line = in.readLine();
+	         cfg.parse(line, lineNumber);
+	      }
+	   } catch(IOException e) {
+	      /* IOException should be thrown on the subsequent read after all of
+	       * the data has been consumed, so we can safely ignore it.
+	       */
+	   }
 	   System.out.println(cfg.toString());
 	}
 }
+
+//TODO: single line control statements, 2+ predicates
